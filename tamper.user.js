a0664d19278936e617b6873012646e405bf91d51
Katelyn Slater <ktaeyln@gmail.com>
2016-05-26 07:39:24 -0400

    Update tamper.user.js

--- tamper.user.js
+++ tamper.user.js
@@ -15,10 +15,10 @@
 
 window.addEventListener("load", function () {
     var script = document.createElement("SCRIPT");
-    script.src = "https://cdn.rawgit.com/QtSlitherBot/QtSlitherBot/master/basic_bot.js";
+    script.src = "https://cdn.rawgit.com/QtSlitherBot/QtSlitherBot/3b5a9c30d54f0cf945b6bd514849f3809b81a709/basic_bot.js";
     document.getElementsByTagName('head')[0].appendChild(script);
 
     script = document.createElement("SCRIPT");
-    script.src = "https://cdn.rawgit.com/QtSlitherBot/QtSlitherBot/master/bot_framework.js";
+    script.src = "https://cdn.rawgit.com/QtSlitherBot/QtSlitherBot/3b5a9c30d54f0cf945b6bd514849f3809b81a709/bot_framework.js";
     document.getElementsByTagName('head')[0].appendChild(script);
 });
